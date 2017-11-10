af402a3a2b2d22c48f78940391439f3632f77ec0
Che1 <nachwon@gmail.com>
2017-11-10 18:31:29 +0900

    refactored

--- hy_tracker/slackapi/views.py
+++ hy_tracker/slackapi/views.py
@@ -1,35 +1,54 @@
 import requests
 import time
+
+import sys
+
+from django.http import HttpResponse
 from django.shortcuts import render
 import json
+
+key = '032E05FE0635F1828FC936595667CABA'
+steam_id = '76561198005689159'
+
+
+def steamapi_status(key, steam_id):
+    api_url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={steam_id}'
+    response = requests.get(api_url)
+    data = json.loads(response.content)
+    hy_status = data['response']['players'][0]['personastate']
+    return hy_status
+
+
+def slackapi_post(api_url, message):
+    message = {"text": message}
+    requests.post(api_url, json.dumps(message), headers={'Content-Type': 'application/json'})
+
+
+def loop(count):
+    while count == 0:
+        hy_status = steamapi_status(key, steam_id)
+        if hy_status == 1:
+            print(hy_status)
+            message = "이한영 강사님 배틀그라운드 접속"
+            slack_url = 'https://hooks.slack.com/services/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'
+            slackapi_post(slack_url, message)
+            print(count)
+        else:
+            print(hy_status)
+        time.sleep(5)
 
 
 def index(request):
     if request.method == 'POST':
-        status = request.POST.get('status')
         switch = request.POST.get('switch')
-        if status == '1':
-            message = {"text": "이한영 강사님 배틀그라운드 접속"}
-            requests.post(
-            'https://hooks.slack.com/services/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',
-            json.dumps(message),
-            headers={'Content-Type': 'application/json'})
-        elif status == '2':
-            response = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=032E05FE0635F1828FC936595667CABA&steamids=76561198005689159')
-            data = json.loads(response.content)
+        print(switch)
 
-        while True:
-            if switch == 'on':
-                message = {"text": "잘됨"}
-                response = requests.get(
-                    'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=032E05FE0635F1828FC936595667CABA&steamids=76561198005689159')
-                data = json.loads(response.content)
-                print(data)
-                print(data['response']['players'][0]['personastate'])
-                time.sleep(5)
-            else:
-                break
-
+        if switch == 'on':
+            loop(0)
+    else:
+        pass
     return render(request, 'index.html')
+
+
 
 
