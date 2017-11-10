d54698e825148d4ee38ccc62cdd7a6a4b361f7bd
Che1 <nachwon@gmail.com>
2017-11-10 16:22:55 +0900

    loop added

--- hy_tracker/slackapi/views.py
+++ hy_tracker/slackapi/views.py
@@ -1,11 +1,13 @@
 import requests
+import time
 from django.shortcuts import render
 import json
 
 
 def index(request):
     if request.method == 'POST':
-        status = request.POST['status']
+        status = request.POST.get('status')
+        switch = request.POST.get('switch')
         if status == '1':
             message = {"text": "이한영 강사님 배틀그라운드 접속"}
             requests.post(
@@ -15,6 +17,18 @@ def index(request):
         elif status == '2':
             response = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=032E05FE0635F1828FC936595667CABA&steamids=76561198005689159')
             data = json.loads(response.content)
+
+        while True:
+            if switch == 'on':
+                message = {"text": "잘됨"}
+                response = requests.get(
+                    'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=032E05FE0635F1828FC936595667CABA&steamids=76561198005689159')
+                data = json.loads(response.content)
+                print(data)
+                print(data['response']['players'][0]['personastate'])
+                time.sleep(5)
+            else:
+                break
 
     return render(request, 'index.html')
 
