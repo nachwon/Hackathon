c3ddd66ad1992fefe161aa66807d104774e7dec0
Che1 <nachwon@gmail.com>
2017-11-10 16:04:12 +0900

    steam stats and slack message bot added

--- hy_tracker/slackapi/views.py
+++ hy_tracker/slackapi/views.py
@@ -1,3 +1,21 @@
+import requests
 from django.shortcuts import render
+import json
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        status = request.POST['status']
+        if status == '1':
+            message = {"text": "이한영 강사님 배틀그라운드 접속"}
+            requests.post(
+            'https://hooks.slack.com/services/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',
+            json.dumps(message),
+            headers={'Content-Type': 'application/json'})
+        elif status == '2':
+            response = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=032E05FE0635F1828FC936595667CABA&steamids=76561198005689159')
+            data = json.loads(response.content)
+
+    return render(request, 'index.html')
+
+
